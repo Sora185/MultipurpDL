3b88aa7271ce7c18a073f10e933cc4afc5ff29ce
Sora185 <26360856+Sora185@users.noreply.github.com>
2023-05-23 10:02:34 +0200

    Update download_mp3.bat
    - Abschlussnachricht angepasst
    - Abfrage, ob yt-dlp.exe, ffmpeg.exe oder ffprobe.exe fehlen.

--- download_mp3.bat
+++ download_mp3.bat
@@ -4,12 +4,26 @@ chcp 65001 >NUL
 
 cd "%~dp0"
 
+::==============================================
+
+IF NOT EXIST ".\bin\yt-dlp.exe" call :MissingFile yt-dlp.exe
+IF NOT EXIST ".\bin\ffmpeg.exe" call :MissingFile ffmpeg.exe
+IF NOT EXIST ".\bin\ffprobe.exe" call :MissingFile ffprobe.exe
+
+::==============================================
+
 set /p URL="URL: "
 echo.
 .\bin\yt-dlp.exe --ffmpeg-location ".\bin\ffmpeg.exe" -o ".\Downloads\%%(title)s.%%(ext)s" -x --audio-format mp3 --audio-quality 192k --console-title "%URL%"
 echo.
-echo ▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+echo ----------------------------------------------------------------------------------------------------
+echo Vorgang abgeschlossen
 echo.
+pause
+goto :end
+
+:MissingFile
+echo %~1 konnte nicht gefunden werden!
 pause
 goto :end
 
